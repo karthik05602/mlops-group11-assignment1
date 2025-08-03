d64306e27ef410414a5aa2d7f526494458d50d2b
2023ac05602 <2023ac05602@wilp.bits-pilani.ac.in>
2025-08-03 16:34:23 +0530

    Added input validation using flask_pydantic

--- app/app.py
+++ app/app.py
@@ -1,20 +1,25 @@
 from flask import Flask, request, jsonify
+from pydantic import BaseModel, ValidationError
+from flask_pydantic import validate
 import joblib
 import pandas as pd
 import logging
 from prometheus_flask_exporter import PrometheusMetrics
 
+# Define input schema
+class IrisFeatures(BaseModel):
+    SepalLengthCm: float
+    SepalWidthCm: float
+    PetalLengthCm: float
+    PetalWidthCm: float
+
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-
-# Optional: custom metric for prediction endpoint
 metrics.info('app_info', 'Iris prediction API', version='1.0.0')
 
-# Load model and encoder
 model = joblib.load('model.pkl')
 label_encoder = joblib.load('label_encoder.pkl')
 
-# Configure logging to a file
 logging.basicConfig(
     filename='logs/iris_api.log',
     level=logging.INFO,
@@ -22,18 +27,13 @@ logging.basicConfig(
 )
 
 @app.route('/predict', methods=['POST'])
+@validate()
 @metrics.counter('predict_requests_total', 'Total prediction requests')
-def predict():
-    data = request.get_json()
-    input_df = pd.DataFrame([data])
-
-    # Prediction
+def predict(body: IrisFeatures):
+    input_df = pd.DataFrame([body.dict()])
     prediction = model.predict(input_df)
     predicted_label = label_encoder.inverse_transform([prediction[0]])[0]
-
-    # Log input and prediction
-    logging.info(f"Input: {data} | Prediction: {predicted_label}")
-
+    logging.info(f"Input: {body.dict()} | Prediction: {predicted_label}")
     return jsonify({'prediction': predicted_label})
 
 if __name__ == '__main__':
